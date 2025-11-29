4e694568c1e5d8ea1f046285b8a519417dbc960a
anugowda2 <anuvenkatesh0210@gmail.com>
2025-11-29 10:05:37 +0530

    Delete Shopping_CS directory

--- Shopping_CS/src/com/tnsif/anushreev/shoppingcs/ShopFactory.java
+++
@@ -1,6 +0,0 @@
-package com.tnsif.anushreev.shoppingcs;
-
-public abstract class ShopFactory {
-    public abstract PrimeAcc getNewPrimeAccount(int accNo, String accNm, float charges, boolean isPrime);
-    public abstract NormalAcc getNewNormalAccount(int accNo, String accNm, float charges, float deliveryCharges);
-}
